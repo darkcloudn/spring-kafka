c886c25a358554a62125e0c08cd748376e779e03
Viettd <ducvietttong@gmail.com>
2021-10-02 00:04:41 +0700

    update consumer

---
+++ library-events-consumer/src/main/java/com/learnkafka/service/LibraryEventsService.java
@@ -0,0 +1,63 @@
+package com.learnkafka.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.entity.LibraryEvent;
+import com.learnkafka.jpa.LibraryEventsRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class LibraryEventsService {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    private LibraryEventsRepository libraryEventsRepository;
+
+    public void processLibraryEvent(ConsumerRecord<Integer,String> consumerRecord) throws JsonProcessingException {
+        LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
+        log.info("libraryEvent : {} ", libraryEvent);
+
+        switch(libraryEvent.getLibraryEventType()){
+            case NEW:
+                save(libraryEvent);
+                break;
+            case UPDATE:
+                //valid
+                validation(libraryEvent);
+                //update operation
+                save(libraryEvent);
+                break;
+            default:
+                log.info("Invalid Library Event Type");
+        }
+
+    }
+
+    private void update(LibraryEvent libraryEvent) {
+
+    }
+
+    private void validation(LibraryEvent libraryEvent) {
+        if(libraryEvent.getLibraryEventId()==null)
+            throw new IllegalArgumentException("Library Event id is missing");
+        Optional<LibraryEvent> libraryEventOptional = libraryEventsRepository.findById(libraryEvent.getLibraryEventId());
+        if(!libraryEventOptional.isPresent())
+            throw new IllegalArgumentException("Not Valid Library Event");
+        log.info("Validation is success LibraryEvent={}",libraryEventOptional.get());
+
+    }
+
+    private void save(LibraryEvent libraryEvent) {
+        libraryEvent.getBook().setLibraryEvent(libraryEvent);
+        libraryEventsRepository.save(libraryEvent);
+        log.info("Successfully Persisted the libary Event {} ", libraryEvent);
+    }
+}
